551929865f69967021a03c88d2885707d5c56b9d
gmtalha21 <gmtalha9@gmail.com>
2023-07-29 18:38:27 +0500

    Setup vuejs and tailwind css

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
 import './bootstrap';
+import { createApp } from 'vue';
+
+const app = createApp({});
+
+import Welcome from './pages/Welcome.vue';
+
+/* Register vue components */
+app.component('welcome-component', Welcome);
+
+app.mount('#app');
---
+++ resources/js/pages/Welcome.vue
@@ -0,0 +1,15 @@
+<template>
+  <p class="text-white">Welcome</p>
+</template>
+
+<script>
+export default {
+    mounted() {
+        console.log("component mounted")
+    }
+}
+</script>
+
+<style>
+
+</style>
