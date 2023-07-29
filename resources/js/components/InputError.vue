110181fac7f133137f245a2f27787a5ea07aa8d6
gmtalha21 <gmtalha9@gmail.com>
2023-07-29 22:21:48 +0500

    Add Helper components

---
+++ resources/js/components/InputError.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps({
+    message: {
+        type: Array,
+    },
+});
+</script>
+
+<template>
+    <div v-show="message">
+        <p class="text-sm text-red-600">
+            {{ message[0] }}
+        </p>
+    </div>
+</template>
