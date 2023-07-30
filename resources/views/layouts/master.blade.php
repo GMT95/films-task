f22d8fb1f076a45e7f0e9c908e7448253111a8f7
gmtalha21 <gmtalha9@gmail.com>
2023-07-31 00:26:01 +0500

    Add Navbar links

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -16,23 +16,43 @@
         @yield('scripts')
     </head>
     <body class="font-sans antialiased">
-        {{-- <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center dark:bg-dots-lighter selection:bg-red-500 selection:text-white">
-            @if (Route::has('login'))
-                <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
-                    @auth
-                        <a href="{{ url('/home') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Home</a>
-                    @else
-                        <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
+        <div class="border-2 bg-white sticky top-0 h-16">
+            <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
+                @auth
+                    <span
+                        class="mr-2 border-r-2 p-2 font-semibold text-gray-400 hover:text-gray-600"
+                    >
+                        Hi, Jason
+                    </span>
+                    <a
+                        href="{{ route('films.list.page') }}"
+                        class="mr-2 font-semibold text-gray-400 hover:text-gray-600"
+                    >
+                        Films
+                    </a>
+                    <a
+                        href="{{ route('logout.web') }}"
+                        class="font-semibold text-gray-400 hover:text-gray-600"
+                    >
+                        Logout
+                    </a>
+                @else
+                    <a
+                        href="{{ route('login.page') }}"
+                        class="mr-2 font-semibold text-gray-400 hover:text-gray-600"
+                    >
+                        Log in
+                    </a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-
-        </div> --}}
+                    <a
+                        href="{{ route('register.page') }}"
+                        class="font-semibold text-gray-400 hover:text-gray-600"
+                    >
+                        Register
+                    </a>
+                @endauth
+            </div>
+        </div>
 
         <div id="app">
             @yield('content')
